9128a3c1c045a33e72d33711b26fb11bb1bd9785
gamzeisl <gamzeisl@hotmail.com>
2025-04-15 17:02:45 +0200

    Change default vector size

--- src/fpnew_sdotp_scale_multi_modules.sv
+++ src/fpnew_sdotp_scale_multi_modules.sv
@@ -19,7 +19,7 @@ package fpnew_sdotp_scale_multi_pkg;
   // One-hot config string: | FP32 | FP64 | FP16 | FP8 | FP16ALT | FP8ALT | FP6 | FP6ALT | FP4
   localparam fpnew_pkg::fmt_logic_t   SrcDotpFpFmtConfig = 9'b000101111; // Supported source formats (FP8, FP8ALT, FP6, FP6ALT, FP4)
   localparam fpnew_pkg::fmt_logic_t   DstDotpFpFmtConfig = 9'b100010000; // Supported destination formats (FP32)
-  localparam int unsigned             VectorSize = `ifdef VECTOR_SIZE `VECTOR_SIZE `else 4 `endif;
+  localparam int unsigned             VectorSize = `ifdef VECTOR_SIZE `VECTOR_SIZE `else 8 `endif;
   localparam int unsigned             NumPipeRegs = `ifdef NUM_PIPE_REGS `NUM_PIPE_REGS `else 3 `endif;
   localparam fpnew_pkg::pipe_config_t PipeConfig  = fpnew_pkg::BEFORE;
   localparam type                     TagType     = logic;
